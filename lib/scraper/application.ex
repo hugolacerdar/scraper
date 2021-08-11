8126cce93618f83ecac53166cb09715a52b71aff
Hugo <hugolacerdar@gmail.com>
2021-08-11 08:51:04 -0300

    Update format

--- lib/scraper/application.ex
+++ lib/scraper/application.ex
@@ -3,7 +3,10 @@ defmodule Scraper.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [PageProducer, PageConsumer]
+    children = [
+      PageProducer,
+      PageConsumer
+    ]
 
     opts = [strategy: :one_for_one, name: Scraper.Supervisor]
     Supervisor.start_link(children, opts)
